7897b45fb226530b53a45662e354f91a6f205492
Chris Coffey <christopher.coffey@rackspace.com>
2014-01-14 15:37:04 -0600

    changed attribute names to meet new format

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,12 +1,14 @@
 #
-# Cookbook Name:: ntp
+# Cookbook Name:: rackspace_ntp
 # Attributes:: default
 #
 # Author:: Joshua Timberman (<joshua@opscode.com>)
 # Author:: Tim Smith (<tsmith@limelight.com>)
 # Author:: Charles Johnson (<charles@opscode.com>)
+# Author:: Christopher Coffey (<christopher.coffey@rackspace.com>)
 #
 # Copyright 2009-2013, Opscode, Inc.
+# Copyright 2014, Rackspace, US, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,49 +24,35 @@
 #
 
 # default attributes for all platforms
-default['ntp']['servers']   = [] # The default recipe sets a list of common NTP servers (COOK-1170)
-default['ntp']['peers'] = []
-default['ntp']['restrictions'] = []
+default[:rackspace_ntp][:config][:servers] = [] 
+default[:rackspace_ntp][:config][:peers] = []
+default[:rackspace_ntp][:config][:restrictions] = []
 
 # internal attributes
-default['ntp']['packages'] = %w(ntp ntpdate)
-default['ntp']['service'] = 'ntpd'
-default['ntp']['varlibdir'] = '/var/lib/ntp'
-default['ntp']['driftfile'] = "#{node['ntp']['varlibdir']}/ntp.drift"
-default['ntp']['conffile'] = '/etc/ntp.conf'
-default['ntp']['statsdir'] = '/var/log/ntpstats/'
-default['ntp']['conf_owner'] = 'root'
-default['ntp']['conf_group'] = 'root'
-default['ntp']['var_owner'] = 'ntp'
-default['ntp']['var_group'] = 'ntp'
-default['ntp']['leapfile'] = '/etc/ntp.leapseconds'
-default['ntp']['sync_clock'] = false
-default['ntp']['sync_hw_clock'] = false
-default['ntp']['listen'] = nil
-default['ntp']['listen_network'] = nil
-default['ntp']['apparmor_enabled'] = false
+default[:rackspace_ntp][:packages] = %w(ntp ntpdate)
+default[:rackspace_ntp][:service] = 'ntpd'
+default[:rackspace_ntp][:varlibdir] = '/var/lib/ntp'
+default[:rackspace_ntp][:conffile] = '/etc/ntp.conf'
+default[:rackspace_ntp][:conf_owner] = 'root'
+default[:rackspace_ntp][:conf_group] = 'root'
+default[:rackspace_ntp][:var_owner] = 'ntp'
+default[:rackspace_ntp][:var_group] = 'ntp'
+default[:rackspace_ntp][:sync_clock] = false
+default[:rackspace_ntp][:sync_hw_clock] = false
+default[:rackspace_ntp][:listen_network] = nil
+default[:rackspace_ntp][:apparmor_enabled] = false
+
+default[:rackspace_ntp][:config][:driftfile] = "#{node[:rackspace_ntp][:varlibdir]}/ntp.drift"
+default[:rackspace_ntp][:config][:statsdir] = '/var/log/ntpstats/'
+default[:rackspace_ntp][:config][:leapfile] = '/etc/ntp.leapseconds'
+default[:rackspace_ntp][:config][:listen] = nil
+
 
 # overrides on a platform-by-platform basis
-case node['platform_family']
+case node[:platform_family]
 when 'debian'
-  default['ntp']['service'] = 'ntp'
-  default['ntp']['apparmor_enabled'] = true if node['platform'] == 'ubuntu' && node['platform_version'].to_f >= 8.04
+  default[:rackspace_ntp][:service] = 'ntp'
+  default[:rackspace_ntp][:apparmor_enabled] = true if node[:platform] == 'ubuntu' && node[:platform_version].to_f >= 8.04
 when 'rhel'
-  default['ntp']['packages'] = %w(ntp) if node['platform_version'].to_i < 6
-when 'windows'
-  default['ntp']['service'] = 'NTP'
-  default['ntp']['driftfile'] = 'C:\\NTP\\ntp.drift'
-  default['ntp']['conffile'] = 'C:\\NTP\\etc\\ntp.conf'
-  default['ntp']['conf_owner'] = 'Administrators'
-  default['ntp']['conf_group'] = 'Administrators'
-  default['ntp']['package_url'] = 'http://www.meinbergglobal.com/download/ntp/windows/ntp-4.2.6p5@london-o-lpv-win32-setup.exe'
-  default['ntp']['vs_runtime_url'] = 'http://download.microsoft.com/download/1/1/1/1116b75a-9ec3-481a-a3c8-1777b5381140/vcredist_x86.exe'
-  default['ntp']['vs_runtime_productname'] = 'Microsoft Visual C++ 2008 Redistributable - x86 9.0.21022'
-when 'freebsd'
-  default['ntp']['packages'] = %w(ntp)
-  default['ntp']['varlibdir'] = '/var/db'
-  default['ntp']['driftfile'] = "#{node['ntp']['varlibdir']}/ntpd.drift"
-  default['ntp']['statsdir'] = "#{node['ntp']['varlibdir']}/ntpstats"
-  default['ntp']['conf_group'] = 'wheel'
-  default['ntp']['var_group'] = 'wheel'
+  default[:rackspace_ntp][:packages] = %w(ntp) if node[:platform_version].to_i < 6
 end
