13a6fe5363a95ca5eb6951af55b14a3950860d08
Ryan Richard <ryanrichard07@gmail.com>
2014-01-31 00:08:15 -0600

    add standard thor and rake files

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email  'rackspace-cookbooks@rackspace.com'
 license           'Apache 2.0'
 description       'Installs and configures ntp as a client or server'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '2.0.0'
+version           '2.0.1'
 
 recipe 'rackspace_ntp', 'Installs and configures ntp either as a server or client'
